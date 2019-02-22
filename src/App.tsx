79a1a5fb8e6b57ac09e616c0ea88f4460f66cb71
YOULYU ZHANG <youlyuzhang.work@gmail.com>
2019-02-22 18:15:42 +0900

    add typing file, first example works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import AxisOn0 from "./components/example/LineChart";
 
 class App extends Component {
     render() {
         return (
             <div>
                 Hello.
+                <AxisOn0 />
             </div>
         );
     }
